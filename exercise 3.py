7aaeeb9034486887386464e2853322f4129040ff
Ashrafbinshiful9 <ashrafbinshiful@gmail.com>
2024-11-24 21:09:27 +0400

    Update exercise 3.py

--- exercise 3.py
+++ exercise 3.py
@@ -1,9 +1,6 @@
 #exercise3
-name = input("Enter your name:")
-hometown = input("Enter your hometown:")
+name = input("Enter your name: ")
+hometown = input("Enter your hometown: ")
 age = input("Enter your age: ")
 
-
-print("Name:", name)
-print("Hometown:", hometown)
-print("Age:", age)
+print("Name:"+ name +",Hometown:"+ hometown +",Age:"+age)
