2b317fee64ad386f510f994e3e55bf0107ce99e0
Ashrafbinshiful9 <ashrafbinshiful@gmail.com>
2024-11-24 21:14:12 +0400

    Update exercise 8.py

--- exercise 8.py
+++ exercise 8.py
@@ -1,11 +1,9 @@
 #exercise8
+names = ["Ashraf", "harneet", "aryan", "rahim", "afsar", "ali"] 
 
-names = ["Ashraf", "harneet", "aryan","rahim","afsar","ali"] 
-
-search_name = input("Enter the name to search: ")
-
+search_name = input("Enter name to search: ")
 
 if search_name in names:
-    print(f"'{search_name}' name is on the list.")
+    print("'" + search_name + "' name is on the list.")
 else:
-    print(f"'{search_name}' the name is not found in the list.")
+    print("'" + search_name + "' name is not in the list.")
