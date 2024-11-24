901b1da3077af35df86b643afd0356bff421d9e3
Ashrafbinshiful9 <ashrafbinshiful@gmail.com>
2024-11-24 21:20:51 +0400

    Update exercise 6.py

--- exercise 6.py
+++ exercise 6.py
@@ -1,17 +1,16 @@
 #exercise6
 correct_password = "1974"
-
 attempts_left = 5
 
 while attempts_left > 0:
     password = input("Enter the password: ")
-    
+
     if password == correct_password:
-        print("Unlocked ")
+        print("Unlocked")
         break
     else:
         attempts_left -= 1
-        print(f"Wrong password. You have {attempts_left} attempts left.")
+        print("Wrong password. You have", attempts_left, "attempts left.")
 
 if attempts_left == 0:
-    print("try again in 5 minutes.")
+    print("Try again in 5 minutes.")
